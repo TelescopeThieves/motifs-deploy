3dfd16a0a4c58dd8298c34e5002cd97cc3559720
Mario De Los Santos <mario.jdls@gmail.com>
2021-08-20 11:44:16 -0400

    landing page update

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -11,7 +11,7 @@ const Button = (props) => {
         <Link to={props.link}>
             <button className={props.classes} type="submit" onClick={props.onClick} value={props.value}>
                 {props.svg}
-                <span className='hidden margin-left'>{props.name}</span>
+                <span className='margin-left'>{props.name}</span>
             </button>
         </Link>       
     )
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,27 +1,69 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, {useState, useContext } from 'react'
+import {Link, Redirect} from "react-router-dom";
+import { UserContext } from '../Context/UserContext'
+import axios from 'axios'
+import Button from './Button';
+
 
 const Landing = () => {
+    const {loggedInUserContext, setLoggedInUserContext} = useContext(UserContext)
+    const [redirect, setRedirect] = useState(false)
+    const guest = {
+        email: "guest@guest.com",
+        password: "12341234"
+    }
+    const guestLogin = async () => {
 
+        const res = await axios.post('/auth/loginUser', guest)
+        
+        console.log(res.data)
+        
+        const {refreshtoken, accesstoken, userId} = res.data
+
+        if(refreshtoken){
+            setLoggedInUserContext({refreshtoken, accesstoken, userId})
+            setRedirect(true)
+        }else{
+            console.log('whoops')
+        }
+    }
+    if(redirect){
+        return (
+            <Redirect to='/feed' />
+        )
+    }
     return(
         <section className="gradient landing">
-            <section className="audioAndArt index stream radius padding">
+            <section className="">
                 
-                <section>
+                <section className='width100 padding'>
                     <h1 className="darkGrey bigFont">Welcome to Motifs!</h1>
                 </section>
+                <div className='padding'>
+                    <p>The world's only ephemeral audio site! All audio posted will self-destruct from the site in 24 hours. </p>
+                </div>
 
-                <section className="indexBtns">
+                <section className="indexBtns padding">
 
                     <div className="centerText margin-right">
                         <Link to='/signup'>
                             <button type="submit" className="trackBtn padding-top padding-bottom">Create Account</button>
                         </Link>
                     </div>
-                    <div className="centerText margin-left">
+                    <div className="centerText margin-right">
                         <Link to='/login'>
                             <button type="submit" className="trackBtn padding-top padding-bottom">Login</button>
                         </Link>
+                    </div>
+                    <div className="centerText">
+                        
+                        <Button 
+                        classes='trackBtn padding-top padding-bottom'
+                        onClick={guestLogin}
+                        value='Guest Login'
+                        name='Guest Login'
+                        />
+
                     </div>
                 </section>
             </section>
